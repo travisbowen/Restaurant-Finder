0d73ccc4c188a7cb811a0f5556fe850f3f088b7b
Travis Bowen <Travisbowen23@gmail.com>
2020-10-20 19:09:51 -0700

    Sort restaurants data alphabetically
    Created basic restaurant table component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import RestaurantsTable from "./components/RestaurantsTable";
 
 function App() {
+	const [restaurants, setRestaurants] = useState([]);
+
 	// Runs only once to get data
 	useEffect(() => {
-		fetch("https://code-challenge.spectrumtoolbox.com/api/restaurants", {
-			headers: {
-				Authorization: "Api-Key q3MNxtfep8Gt",
-			},
-		})
-			.then((response) => response.json())
-			.then((data) => {
-				console.log(data);
-			});
+		const getRestaurantsData = async () => {
+			await fetch(
+				"https://code-challenge.spectrumtoolbox.com/api/restaurants",
+				{
+					headers: {
+						Authorization: "Api-Key q3MNxtfep8Gt",
+					},
+				},
+			)
+				.then((response) => response.json())
+				.then((data) => {
+					data.sort(sortRestaurants);
+					setRestaurants(data);
+				});
+		};
+
+		getRestaurantsData();
 	}, []);
+
+	// Sorts restaurants alphabetically
+	function sortRestaurants(a, b) {
+		// Use toUpperCase() to ignore character casing
+		const nameA = a.name.toUpperCase();
+		const nameB = b.name.toUpperCase();
+
+		let comparison = 0;
+		if (nameA > nameB) {
+			comparison = 1;
+		} else if (nameA < nameB) {
+			comparison = -1;
+		}
+		return comparison;
+	}
 
 	return (
 		<div className='app'>
-			<h1>App</h1>
+			<h1>Restaurant Finder</h1>
+			<RestaurantsTable restaurants={restaurants} />
 		</div>
 	);
 }
---
+++ src/components/RestaurantsTable.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./RestaurantsTable.css";
+
+function RestaurantsTable(props) {
+	return (
+		<div className='restaurants__table'>
+			<h1>Restaurants Data</h1>
+		</div>
+	);
+}
+
+export default RestaurantsTable;
