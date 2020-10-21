62a299fe95c918f71ca6c30c5a4d4a94e0f787b4
Travis Bowen <Travisbowen23@gmail.com>
2020-10-20 18:03:35 -0700

    Fetching restaurant data from api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 
 function App() {
+	// Runs only once to get data
+	useEffect(() => {
+		fetch("https://code-challenge.spectrumtoolbox.com/api/restaurants", {
+			headers: {
+				Authorization: "Api-Key q3MNxtfep8Gt",
+			},
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+			});
+	}, []);
+
 	return (
 		<div className='app'>
 			<h1>App</h1>
